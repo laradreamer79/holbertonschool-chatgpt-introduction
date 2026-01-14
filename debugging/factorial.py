26ac8a26712ebafff7ccf5d4869909b77019e7d7
laradreamer79 <laradreamer79@gmail.com>
2026-01-14 08:45:08 +0000

    fix factorial fuction code

---
+++ debugging/factorial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
+
